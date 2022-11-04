2dedf476ad44237e9ae6a34544aee1dd241b0fc9
hani Khellafi <hanikhellafi@gmail.com>
2022-11-04 13:27:13 -0500

    first commit

---
+++ src/main/java/com/learning/cogbankapp/CogBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.cogbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/cogbankapp/CogBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.cogbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
