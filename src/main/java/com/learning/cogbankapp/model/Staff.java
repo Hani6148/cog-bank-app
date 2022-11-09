c6f3d750f006ab609d156db2da4c9ce6e89daafb
satyank <satyank.nadimpalli@outlook.com>
2022-11-09 07:00:36 -0600

    Created Staff Model Class

--- src/main/java/com/learning/cogbankapp/model/Staff.java
+++ src/main/java/com/learning/cogbankapp/model/Staff.java
@@ -1,6 +1,27 @@
 package com.learning.cogbankapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "staff_table")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Staff {
+	@Id
+	@GeneratedValue
 	private int id;
-	private String name;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
 }
