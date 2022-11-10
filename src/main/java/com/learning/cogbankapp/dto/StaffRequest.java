4c3589c9c16efca462a803d19f96bd4c00ad11ab
satyank <satyank.nadimpalli@outlook.com>
2022-11-10 10:35:36 -0600

    Updated Staff DTO

--- src/main/java/com/learning/cogbankapp/dto/StaffRequest.java
+++ src/main/java/com/learning/cogbankapp/dto/StaffRequest.java
@@ -14,4 +14,42 @@ public class StaffRequest {
 	private String email;
 	@Size(min = 6, message = "Password is too short.")
 	private String password;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "StaffRequest [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+
 }
