76d95108ab8a7eaae2cab3ab866f1eaf40497789
satyank <satyank.nadimpalli@outlook.com>
2022-11-12 13:29:09 -0600

    Created StaffServiceValidation Implementation

--- src/main/java/com/learning/cogbankapp/serviceValidationImpl/StaffServiceValidationImpl.java
+++ src/main/java/com/learning/cogbankapp/serviceValidationImpl/StaffServiceValidationImpl.java
@@ -1,5 +1,42 @@
 package com.learning.cogbankapp.serviceValidationImpl;
 
-public class StaffServiceValidationImpl {
+import java.util.List;
+import java.util.Optional;
+
+import com.learning.cogbankapp.dto.StaffRequest;
+import com.learning.cogbankapp.model.Staff;
+import com.learning.cogbankapp.serviceValidation.StaffServiceValidation;
+
+public class StaffServiceValidationImpl implements StaffServiceValidation {
+
+	@Override
+	public Staff registerStaff(StaffRequest staff) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Staff updateStaff(StaffRequest staff) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Optional<Staff> findStaffById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<Staff> findAllStaff() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
