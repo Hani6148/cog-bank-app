5a181dad42dad513e48abbf41a06c219cc4fb72f
satyank <satyank.nadimpalli@outlook.com>
2022-11-12 13:12:30 -0600

    Updated StaffServiceValidation Interface

--- src/main/java/com/learning/cogbankapp/serviceValidation/StaffServiceValidation.java
+++ src/main/java/com/learning/cogbankapp/serviceValidation/StaffServiceValidation.java
@@ -1,5 +1,18 @@
 package com.learning.cogbankapp.serviceValidation;
 
-public interface StaffServiceValidation {
+import java.util.List;
+import java.util.Optional;
 
+import com.learning.cogbankapp.model.Staff;
+
+public interface StaffServiceValidation {
+	public Staff registerStaff(Staff staff);
+
+	public Staff updateStaff(Staff staff);
+
+	public void deleteById(Integer id);
+
+	public Optional<Staff> findStaffById(Integer id);
+
+	public List<Staff> findAllStaff();
 }
