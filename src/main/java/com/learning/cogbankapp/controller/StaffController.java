c50584acd41df48e1ea4c4cffc77b3016248b1ab
satyank <satyank.nadimpalli@outlook.com>
2022-11-16 14:46:57 -0600

    Updated StaffController.java

--- src/main/java/com/learning/cogbankapp/controller/StaffController.java
+++ src/main/java/com/learning/cogbankapp/controller/StaffController.java
@@ -1,5 +1,26 @@
 package com.learning.cogbankapp.controller;
 
-public class StaffController {
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.cogbankapp.dto.StaffRequest;
+import com.learning.cogbankapp.serviceValidation.StaffServiceValidation;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffController {
+	
+	@Autowired
+	StaffServiceValidation staffServiceValidation;
+	
+	@PostMapping("/authenticate")
+	public void authenticate(@Valid @RequestBody StaffRequest staffRequest) {
+		
+	}
+	
 }
