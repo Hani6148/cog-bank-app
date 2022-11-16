106bcfb071f27b1046eccd9198458f7a22992dda
satyank <satyank.nadimpalli@outlook.com>
2022-11-16 14:50:50 -0600

    Updated StaffServiceValidationImpl.java

--- src/main/java/com/learning/cogbankapp/serviceValidationImpl/StaffServiceValidationImpl.java
+++ src/main/java/com/learning/cogbankapp/serviceValidationImpl/StaffServiceValidationImpl.java
@@ -3,16 +3,27 @@ package com.learning.cogbankapp.serviceValidationImpl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.learning.cogbankapp.dto.StaffRequest;
 import com.learning.cogbankapp.model.Staff;
+import com.learning.cogbankapp.repository.StaffRepository;
 import com.learning.cogbankapp.serviceValidation.StaffServiceValidation;
 
 public class StaffServiceValidationImpl implements StaffServiceValidation {
+	
+	@Autowired
+	StaffRepository staffRepository;
 
 	@Override
 	public Staff registerStaff(StaffRequest staff) {
 		// TODO Auto-generated method stub
-		return null;
+		Staff s = new Staff();
+		s.setFirstName(staff.getFirstName());
+		s.setLastName(staff.getLastName());
+		s.setEmail(staff.getEmail());
+		s.setPassword(staff.getPassword());
+		return staffRepository.save(s);
 	}
 
 	@Override
@@ -24,13 +35,13 @@ public class StaffServiceValidationImpl implements StaffServiceValidation {
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public Optional<Staff> findStaffById(Integer id) {
+	public Staff findStaffById(Integer id) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+		return null;
 	}
 
 	@Override
