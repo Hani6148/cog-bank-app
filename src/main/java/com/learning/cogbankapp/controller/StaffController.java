5a22ad2ce1c83ecdcd77c808d65263297914f8a0
satyank <satyank.nadimpalli@outlook.com>
2022-11-17 07:56:08 -0600

    Updated Staff

--- src/main/java/com/learning/cogbankapp/controller/StaffController.java
+++ src/main/java/com/learning/cogbankapp/controller/StaffController.java
@@ -1,5 +1,14 @@
 package com.learning.cogbankapp.controller;
 
-public class StaffController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/staff")
+public class StaffController {
+	@PostMapping("/authenticate")
+	public void authenticateStaff() {
+		
+	}
 }
